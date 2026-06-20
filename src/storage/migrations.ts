6b654ce9ea09cbf1a4920969e9ffe63ec5d07b46
tianpeng <tjys@me.com>
2026-06-20 12:33:54 +0800

    fix: preserve p1 migration state type

--- src/storage/migrations.ts
+++ src/storage/migrations.ts
@@ -1,6 +1,6 @@
 import { ensureP1State } from "../domain/p1/defaultState";
 import type { LifeState } from "../domain/types";
 
-export function migrateLifeState(life: LifeState): LifeState {
+export function migrateLifeState(life: LifeState): ReturnType<typeof ensureP1State> {
   return ensureP1State(life);
 }
